2f7df89c84b380ea635df1683c2dbfa2a211d5f9
phani bandaru <pbndru@gmail.com>
2022-02-21 22:48:44 +0000

    Add project files.

---
+++ Phani.Yield/Program-Before.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace Phani.Yield
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            PrintEmployeeDetails();
+//        }
+
+//        static void PrintEmployeeDetails()
+//        {
+//            var details = GetEmployeeDetails();
+
+//            foreach (var detail in details)
+//            {
+//                Console.WriteLine($"Employee id {detail.Id}");
+//            }
+//        }
+
+//        static IEnumerable<Employee> GetEmployeeDetails()
+//        {
+//            List<Employee> employeeDetails = new List<Employee>();
+
+//            for (int i = 0; i < 50000; i++)
+//            {
+//                employeeDetails.Add(new Employee() { Id = i, Name = $"Employee {i}" });
+//            }
+
+//            return employeeDetails;
+//        }
+//    }
+
+//    class Employee
+//    {
+//        public int Id { get; set; }
+
+//        public string Name { get; set; }
+//    }
+//}
---
+++ Phani.Yield/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phani.Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintEmployeeDetails();
+        }
+
+        static void PrintEmployeeDetails()
+        {
+            var details = GetEmployeeDetails();
+
+            foreach(var detail in details)
+            {
+                Console.WriteLine($"Employee id {detail.Id}");
+            }
+        }
+
+        static IEnumerable<Employee> GetEmployeeDetails()
+        {
+            for (int i = 0; i < 50000; i++)
+            {
+                yield return new Employee() { Id = i, Name = $"Employee {i}" };
+            }
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
